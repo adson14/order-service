736ed86afac89e966fa153915ef1d67ab096fe59
Adson <seu.email@gmail.com>
2025-08-03 18:11:03 +0000

    Consumindo mensagem do kafka para processar o pedido

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('KAFKA_SERVICE_order') private readonly kafkaClient: ClientKafka,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('order_created')
+  handleOrderCreated(@Payload() order: any) {
+    //Preciso implementar a lógica de processamento do pedido aqui
+
+    this.kafkaClient.emit('process-payment', order);
+    return { message: 'Pedido recebido com sucesso!', order };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE_order',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:29092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
