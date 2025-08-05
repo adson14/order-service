d12b1c92777a425e27e5b5672b94b1d64c7ffb0b
Adson Souza <ty.msn@hotmail.com>
2025-08-04 22:57:59 -0300

    Melhorias de conexão

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,40 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: ['kafka:29092'],
-        },
-        consumer: {
-          groupId: 'order-consumer-group',
-        },
-      },
-    },
-  );
+  const maxRetries = 10;
+  let retries = 0;
 
-  await app.listen();
-  console.log('Order service operante');
+  while (retries < maxRetries) {
+    try {
+          const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        AppModule,
+        {
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              brokers: ['kafka:29092'],
+            },
+            consumer: {
+              groupId: 'order-consumer-group',
+            },
+          },
+        },
+      );
+
+      await app.listen();
+      console.log('Order service operante');
+
+      break;
+    } catch (error) {
+      console.error(`Erro ao conectar ao Kafka. Tentativa ${retries + 1} de ${maxRetries}`);
+      retries++;
+      await new Promise((res) => setTimeout(res, 5000)); // espera 5 segundos
+    }
+  }
+
+  if (retries === maxRetries) {
+    console.error('Não foi possível conectar ao Kafka após várias tentativas.');
+    process.exit(1);
+  }
 }
 bootstrap();
